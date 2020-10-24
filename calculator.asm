39f7bb59ffe512ffd94ee458f70aa11f03a7b93c
root <root@localhost.localdomain>
2020-10-24 13:53:15 +0300

    modified:   String.inc 	new file:   calculator 	modified:   calculator.asm

--- calculator.asm
+++ calculator.asm
@@ -5,30 +5,31 @@ section .bss
 	BUFF string
 
 section .data
-	enter_string db "STRING: ", 0
-	print_string db "STR = ", 0
-	print_num db "NUM = ", 0
+	CONST_STR prompt, "STRING: "
+	CONST_STR str_equals, "STR = "
+	CONST_STR num_equals, "NUM = "
 
 section .text
 	global _start
 
 _start:
-	PRINT_STR enter_string
+	PRINT_STR prompt
 	GET_STR string
 
 	STR_TOINT string, num, 10 
-	PRINT_STR print_num
+	PRINT_STR num_equals
 	PRINT_INT num
 	PRINT_STR endl
-	PRINT_STR print_string
+	PRINT_STR str_equals
 	PRINT_STR string
 	PRINT_STR endl
 
 	INT_TOSTR num, string, 10
-	PRINT_STR print_num
+	PRINT_STR num_equals
 	PRINT_INT num
 	PRINT_STR endl
-	PRINT_STR print_string
+	PRINT_STR str_equals
 	PRINT_STR string
 	PRINT_STR endl
+
 	exit
